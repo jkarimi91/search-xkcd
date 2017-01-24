007eb237c399a4400e996c3ebfc4c877653f8037
Jay Karimi <jkarimi91@gmail.com>
2017-01-23 22:50:24 -0800

    improving request response times by making models global

--- search/views.py
+++ search/views.py
@@ -5,7 +5,10 @@ from sklearn.metrics.pairwise import linear_kernel
 from django.conf import settings
 import os.path
 
-# Create your views here.
+model = joblib.load(os.path.join(settings.DATA_DIR, 'model.p'))
+tfidf = joblib.load(os.path.join(settings.DATA_DIR, 'tfidf.p'))
+comics = joblib.load(os.path.join(settings.DATA_DIR, 'comics.p'))
+
 def search(request):
     query = request.GET.get('query')
     if query is None:
@@ -14,10 +17,6 @@ def search(request):
     return render(request, 'list.html', {'search_results': search_results})
 
 def get_search_results(query):
-    model = joblib.load(os.path.join(settings.DATA_DIR, 'model.p'))
-    tfidf = joblib.load(os.path.join(settings.DATA_DIR, 'tfidf.p'))
-    comics = joblib.load(os.path.join(settings.DATA_DIR, 'comics.p'))
-
     query = model.transform([query])
     search_results = linear_kernel(query, tfidf).flatten()
     indices = search_results.argsort()[::-1]
