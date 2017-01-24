b8441c4d8fb3d88560686cf2e3f9d429d3da98f2
Jay Karimi <jkarimi91@gmail.com>
2017-01-23 16:28:11 -0800

    refactoring

--- search/management/commands/fit_model.py
+++ search/management/commands/fit_model.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from nltk import word_tokenize
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import os
 from sklearn.externals import joblib
@@ -12,16 +13,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         comics = joblib.load(os.path.join(settings.DATA_DIR, 'comics.p'))
-        model = TfidfVectorizer(tokenizer=tokenize, stop_words='english',
-                                ngram_range=(1, 2))
-        text_data = [extract_comic_text(c) for c in comics]
-        tfidf = model.fit_transform(text_data)
+        text = [extract_text(c) for c in comics]
+        model = TfidfVectorizer(tokenizer=tokenize, ngram_range=(1, 2))
+        tfidf = model.fit_transform(text)
 
         joblib.dump(model, os.path.join(settings.DATA_DIR, 'model.p'))
         joblib.dump(tfidf, os.path.join(settings.DATA_DIR, 'tfidf.p'))
         self.stdout.write(self.style.SUCCESS('Successfully fitted model'))
 
-def extract_comic_text(comic):
+def extract_text(comic):
     text = [comic['transcript']]
     if comic['alt'] not in comic['transcript']:
         text.append(comic['alt'])
@@ -30,16 +30,9 @@ def extract_comic_text(comic):
     return ' '.join(text)
 
 def tokenize(text):
-    tokens = word_tokenize(text)
-    return normalize(tokens)
-
-def normalize(tokens):
-    normalized_tokens = []
+    tokens = word_tokenize(text.lower())
     stemmer = PorterStemmer()
-    exclude = set(punctuation)
-    for t in tokens:
-        if t not in exclude:
-            t = stemmer.stem(t.lower())
-            normalized_tokens.append(t)
-    return normalized_tokens
+    stop_words = stopwords.words('english')
+    exclude = set(punctuation).union(set(stop_words))
+    return [stemmer.stem(t) for t in tokens if t not in exclude]
 
